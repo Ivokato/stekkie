fce176eec5ccb248f31bf7981074cc3d57b78cad
Ivo de Kler <ivo.de.kler@gmail.com>
2023-04-17 11:14:26 +0200

    Hello git

---
+++ index.html.js
@@ -0,0 +1,14 @@
+export default `<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <meta name="MobileOptimized" content="width"/>
+  <meta name="HandheldFriendly" content="true"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link rel="stylesheet" media="all" href="/style.css"/>
+</head>
+<body>
+  <main>{{content}}</main>
+</body>
+</html>
+`;
---
+++ index.js
@@ -0,0 +1,26 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import serve from 'koa-static';
+
+import indexHtml from './index.html.js';
+
+const router = new Router();
+
+const PORT = process.env.PORT || 80;
+
+const app = new Koa();
+
+// keys are needed when doing stuff with cookies
+app.keys = ['one key'];
+
+router.get('/', ctx => {
+  ctx.body = indexHtml;
+});
+
+app
+.use(serve('./public'))
+.use(router.routes())
+.use(router.allowedMethods());
+
+app.listen(PORT);
+console.log(`test-pwa listening at ${PORT}`);
