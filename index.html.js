986c0ffc4123658aa35084e4f50c41853da536ef
Ivo de Kler <ivo.de.kler@gmail.com>
2023-04-17 11:45:57 +0200

    Add service worker

--- index.html.js
+++ index.html.js
@@ -23,6 +23,19 @@ export default `<!DOCTYPE html>
 </head>
 <body>
   <main>Dit is die {{content}}</main>
+  <script>
+    if ('serviceWorker' in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('Service worker registration succeeded:', registration);
+      }, /*catch*/ function(error) {
+        console.log('Service worker registration failed:', error);
+      });
+    } else {
+      console.log('Service workers are not supported.');
+    }
+  </script>
 </body>
 </html>
 `;
