a184afa833f6998213e310a7e0d9d73a4b19f0fc
Ivo de Kler <ivo.de.kler@gmail.com>
2023-04-17 11:35:40 +0200

    Added manifest

--- index.html.js
+++ index.html.js
@@ -1,10 +1,24 @@
 export default `<!DOCTYPE html>
 <html lang="nl" dir="ltr">
 <head>
+  <link rel="manifest" href="manifest.json">
+
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="application-name" content="PWA-Test">
+  <meta name="apple-mobile-web-app-title" content="PWA-Test">
+  <meta name="theme-color" content="#123456">
+  <meta name="msapplication-navbutton-color" content="#123456">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta name="msapplication-starturl" content="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="icon" type="image/png" sizes="192x192" href="/testIcon.png">
+  <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/testIcon.png">
+
   <meta charset="utf-8"/>
   <meta name="MobileOptimized" content="width"/>
   <meta name="HandheldFriendly" content="true"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="stylesheet" media="all" href="/style.css"/>
 </head>
 <body>
