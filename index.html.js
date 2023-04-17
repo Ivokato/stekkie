2f77fc3ef542765dbd0d03563737f48619d1c6f4
Ivo de Kler <ivo.de.kler@gmail.com>
2023-04-17 12:07:05 +0200

    Added install button

--- index.html.js
+++ index.html.js
@@ -35,6 +35,44 @@ export default `<!DOCTYPE html>
     } else {
       console.log('Service workers are not supported.');
     }
+
+    // Initialize deferredPrompt for use later to show browser install prompt.
+    let deferredPrompt;
+
+    let button;
+
+    function showInstallPromotion() {
+      button = document.createElement('button');
+      button.innerHTML = 'Install';
+
+      button.addEventListener('click', async () => {
+        // Hide the app provided install promotion
+        hideInstallPromotion();
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        const { outcome } = await deferredPrompt.userChoice;
+
+        // We've used the prompt, and can't use it again, throw it away
+        deferredPrompt = null;
+      });
+
+      document.body.appendChild(button);
+    }
+
+    function hideInstallPromotion() {
+      button.parentNode.removeChild(button);
+    }
+
+    window.addEventListener('beforeinstallprompt', e => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+
+      // Update UI notify the user they can install the PWA
+      showInstallPromotion();
+    });
   </script>
 </body>
 </html>
