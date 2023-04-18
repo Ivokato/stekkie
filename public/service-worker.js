a4f8e76eba6ea946188caae6225c296dba1af684
Ivo de Kler <ivo.de.kler@gmail.com>
2023-04-18 14:26:38 +0200

    Service worker: caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,35 @@
-self.addEventListener('fetch', function(event) {
+const cacheName = 'cache-v1';
+
+const resources = [
+  '/',
+  '/privacy-policy',
+  '/lib.js',
+  '/pwa.js',
+  '/style.css',
+  '/app.js',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => Promise.all(resources.map(url => cache.add(url))))
+  );
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(cacheNames.map(name => {
+      // Delete cache entries for old version
+      if (name !== cacheName) {
+        return caches.delete(name);
+      }
+    })))
   );
 });
