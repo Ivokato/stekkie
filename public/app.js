d06ddc054f59db3c0fb5e765a0e53d8fab61986d
Julius <julius@fitchef.nl>
2023-04-18 13:52:49 +0200

    h2 h3 update

--- public/app.js
+++ public/app.js
@@ -116,14 +116,14 @@ function drawUI() {
       const {length} = cardIds;
       const caption = length && cards[cardIds.at(-1)].caption;
       str += `<div data-stack-name="${name}" class="stack">
-        <h2>${name} (${length})</h2>
+        <h3>${name} (${length})</h3>
         <button data-stack-name="${name}" data-action="delete" class="btn btn-light">remove stack <small class="icon">❌</small></button>
         <div class="form-group">
           <input data-stack-name="${name}" placeholder="Write down your task.." class="form-control" autocapitalize="on"/>
         </div>
         ${length ? (`
         <div class="card row">
-          <h3>${caption}</h3>
+          <h2>${caption}</h2>
           <div class="row buttons">
             ${length > 1 ? `<button data-stack-name="${name}" data-action="postpone" class="btn btn-light">postpone <span class="icon">↧</span></button>` : ''}
             <button data-stack-name="${name}" data-action="finish" class="btn btn-success">done <span class="icon">✓</span></button>
